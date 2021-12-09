854ff68802bf2a72faf814049da1a12a201bd151
jujjh <jhee608@gmail.com>
2021-12-09 23:44:26 +0900

    계산기 추가작업, todolist 진행

--- src/views/part0/index.vue
+++ src/views/part0/index.vue
@@ -1,26 +1,28 @@
 <template>
     <h2>계산기</h2>
+
+    <div class="small">{{calcNum2}}</div>
+
     <div class="calc">
         <div class="input">
             {{calcNum}}
         </div>
         <div class="btnWrap">
             
-            <div class="calcBtn" @click.prevent="numClick('AC')">AC</div>
-            <div class="calcBtn" @click.prevent="numClick('EC')">EC</div>
-            <div class="calcBtn" @click.prevent="numClick('%')">%</div>
-            <div class="calcBtn" @click.prevent="numClick('/')">÷</div>
+            <div class="calcBtn col2" @click.prevent="ac">AC</div>
+            <div class="calcBtn" @click.prevent="percent">%</div>
+            <div class="calcBtn" @click.prevent="calculation('division')">÷</div>
             <div class="numberBtnWrap">
                 <template v-for="(item, index) in numBtn" :key="index">
                 <div class="calcBtn" :class="{'col2': item == 0}" @click.prevent="numClick(item)" >{{item}}</div>
                 </template>
-                <div class="calcBtn" @click.prevent="numClick('.')">.</div>
+                <div class="calcBtn" @click.prevent="decimal">.</div>
             </div>
             <div class="rightBtn">
-                <div class="calcBtn" @click.prevent="numClick('*')">x</div>
-                <div class="calcBtn" @click.prevent="numClick('-')">-</div>
-                <div class="calcBtn" @click.prevent="numClick('+')">+</div>
-                <div class="calcBtn" @click.prevent="numClick('=')">=</div>
+                <div class="calcBtn" @click.prevent="calculation('multiply')">x</div>
+                <div class="calcBtn" @click.prevent="calculation('subtract')">-</div>
+                <div class="calcBtn" @click.prevent="calculation('plus')">+</div>
+                <div class="calcBtn" @click.prevent="result">=</div>
 
             </div>
         </div>
@@ -30,24 +32,64 @@
 <script setup>
 import { ref } from 'vue'
 
-let decimalPont = false
-let calcNum = ref('0');
+
 let numBtn = ref([]);
 for(let i = 9; i >= 0; i--){
     numBtn.value.push(i);
 }
 
-const numClick = (e) => {
-    if(calcNum.value == '0'){
-        calcNum.value = e;
+let calcNum = ref('0');
+let calcNum2 = ref('');
+let calcBtn = false;
+let calcBtnVal = '';
 
-    } else if(e == '.'){
-        let aa = calcNum.value;
-        console.log(aa)
-        
+const numClick = (e) => {
+    if (calcBtn){
+        calcNum2.value += ''+e
     } else{
+        if(calcNum.value == '0'){
+            calcNum.value = e;
+            
+        } else{
+            calcNum.value += ''+e
+        }
+    }
+}
+
+const calculation = (e) =>{
+    calcBtn = true;
+    calcBtnVal = e;
+}
+
+const result = () =>{
+    if(calcBtnVal == 'plus'){
+        calcNum.value = Number(calcNum.value) + Number(calcNum2.value);
+    } else if (calcBtnVal == 'subtract'){
+        calcNum.value = Number(calcNum.value) - Number(calcNum2.value);
+    } else if (calcBtnVal == 'multiply'){
+        calcNum.value = Number(calcNum.value) * Number(calcNum2.value);
+    } else if (calcBtnVal == 'division'){
+        calcNum.value = Number(calcNum.value) / Number(calcNum2.value);
+    }
+    calcNum2.value = 0;
+    calcBtn = false;
+    calcBtnVal = ''
+}
+
+const decimal = () => {
+    let currentNum = calcNum.value;
+    console.log(currentNum)
+    if(currentNum.indexOf('.') === -1){
         calcNum.value += ''+e
     }
+}
+
+const percent = () => {
+    calcNum.value = Number(calcNum.value) * Number(0.01);
+}
+
+const ac = () =>{
+    calcNum.value = 0;
 }
 
 </script>
---
+++ src/views/part0/todo.vue
@@ -0,0 +1,30 @@
+<template>
+    <h2>TODO LIST</h2>
+    <div class="todoListWrap">
+        <div class="inputBox">
+            <input type="text" v-model="inputText" placeholder="일정을 입력하세요">
+            <div class="submit" @click.prevent="submit">등록</div>
+        </div>
+    </div>
+</template>
+<script setup>
+import { ref, reactive } from 'vue'
+
+let inputText = ref(null)
+let listItem = reactive({})
+
+listItem.value = localStorage.getItem('todo');
+
+const submit = ()=>{
+    localStorage.setItem('todo', inputText);
+}
+
+</script>
+
+<style scoped>
+.todoListWrap {background: #eee; padding:30px; border-radius: 30px; min-height: 500px;}
+.inputBox {display: flex; justify-content: space-between; border-bottom:1px solid #fff; padding-bottom:10px;}
+.inputBox [type="text"] {background: transparent; border:0; width:calc(100% - 100px)}
+.inputBox [type="text"]:focus {outline: 0; }
+.inputBox .submit {width:80px; background:#e1e1e1; text-align: center; font-size:14px;}
+</style>
